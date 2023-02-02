d8ee7c940dbb2c848f73f1f882ed7ce9b2c27a7d
DavidAndino <123537730+DavidAndino@users.noreply.github.com>
2023-02-01 20:41:23 -0600

    ejercicio 6

---
+++ PrimerParcial/PrimerParcial/Ejercicio6.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace PrimerParcial
+{
+    partial class Ejercicio6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.arrayListBox = new System.Windows.Forms.ListBox();
+            this.arrayButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // arrayListBox
+            // 
+            this.arrayListBox.FormattingEnabled = true;
+            this.arrayListBox.Location = new System.Drawing.Point(45, 23);
+            this.arrayListBox.Name = "arrayListBox";
+            this.arrayListBox.Size = new System.Drawing.Size(120, 95);
+            this.arrayListBox.TabIndex = 0;
+            // 
+            // arrayButton
+            // 
+            this.arrayButton.Location = new System.Drawing.Point(186, 23);
+            this.arrayButton.Name = "arrayButton";
+            this.arrayButton.Size = new System.Drawing.Size(75, 23);
+            this.arrayButton.TabIndex = 1;
+            this.arrayButton.Text = "Arreglos";
+            this.arrayButton.UseVisualStyleBackColor = true;
+            this.arrayButton.Click += new System.EventHandler(this.arrayButton_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(360, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // Ejercicio6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 224);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.arrayButton);
+            this.Controls.Add(this.arrayListBox);
+            this.Name = "Ejercicio6";
+            this.Text = "Ejercicio6";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox arrayListBox;
+        private System.Windows.Forms.Button arrayButton;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
---
+++ PrimerParcial/PrimerParcial/Ejercicio6.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimerParcial
+{
+    public partial class Ejercicio6 : Form
+    {
+        public Ejercicio6()
+        {
+            InitializeComponent();
+        }
+
+        private void arrayButton_Click(object sender, EventArgs e)
+        {
+            int[] array = new int [5];//creando vector
+            //asignando valores al arreglo
+            array[0] = 10;
+            array[1] = 20;
+            array[2] = 30;
+            array[3] = 40;
+            array[4] = 50;
+
+            //creando e inicializando arreglos
+            int[] array2 = {1,2,3,4,5};
+            int[] array3 = {10,20,30,40,50,60};
+            int[] array4 = new int[] {10,20,40,50};
+
+            string[] array5 = { "Hola", "Mundo", "Cruel"};
+
+            for (int i = 0; i < array5.Length; i++)
+            {
+                arrayListBox.Items.Add(array5[i]);//recorriendo cada una de las posiciones del arreglo y almacenandolo  y mostrandolo en el arrayListBox
+                
+            }
+
+            foreach (string item in array5)//var identifica el tipo de dato automaticamente
+            {
+                comboBox1.Items.Add(item);
+            }
+
+        }
+    }
+}
