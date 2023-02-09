b02e66cc3c532e04c56a69ea9767caac1a36a656
DavidAndino <123537730+DavidAndino@users.noreply.github.com>
2023-02-08 21:01:29 -0600

    2da clase POO

--- PrimerParcial/PrimerParcial/Ejercicio9.Designer.cs
+++ PrimerParcial/PrimerParcial/Ejercicio9.Designer.cs
@@ -28,33 +28,156 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.marcaTextBox = new System.Windows.Forms.TextBox();
+            this.modelTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.precioTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.kmTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.carrosDataGridView = new System.Windows.Forms.DataGridView();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.carrosDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(269, 42);
+            this.button1.Location = new System.Drawing.Point(350, 101);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(112, 35);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Agregar";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 48);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Marca";
+            // 
+            // marcaTextBox
+            // 
+            this.marcaTextBox.Location = new System.Drawing.Point(167, 45);
+            this.marcaTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.marcaTextBox.Name = "marcaTextBox";
+            this.marcaTextBox.Size = new System.Drawing.Size(148, 26);
+            this.marcaTextBox.TabIndex = 2;
+            // 
+            // modelTextBox
+            // 
+            this.modelTextBox.Location = new System.Drawing.Point(167, 77);
+            this.modelTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.modelTextBox.Name = "modelTextBox";
+            this.modelTextBox.Size = new System.Drawing.Size(148, 26);
+            this.modelTextBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 80);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Modelo";
+            // 
+            // precioTextBox
+            // 
+            this.precioTextBox.Location = new System.Drawing.Point(167, 113);
+            this.precioTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.precioTextBox.Name = "precioTextBox";
+            this.precioTextBox.Size = new System.Drawing.Size(148, 26);
+            this.precioTextBox.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(97, 116);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Precio";
+            // 
+            // kmTextBox
+            // 
+            this.kmTextBox.Location = new System.Drawing.Point(167, 149);
+            this.kmTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.kmTextBox.Name = "kmTextBox";
+            this.kmTextBox.Size = new System.Drawing.Size(148, 26);
+            this.kmTextBox.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(97, 152);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "km";
+            // 
+            // carrosDataGridView
+            // 
+            this.carrosDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.carrosDataGridView.Location = new System.Drawing.Point(12, 198);
+            this.carrosDataGridView.Name = "carrosDataGridView";
+            this.carrosDataGridView.Size = new System.Drawing.Size(609, 179);
+            this.carrosDataGridView.TabIndex = 9;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
             // Ejercicio9
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(548, 253);
+            this.ClientSize = new System.Drawing.Size(633, 389);
+            this.Controls.Add(this.carrosDataGridView);
+            this.Controls.Add(this.kmTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.precioTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.modelTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.marcaTextBox);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Ejercicio9";
             this.Text = "Ejercicio9";
+            this.Load += new System.EventHandler(this.Ejercicio9_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.carrosDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox marcaTextBox;
+        private System.Windows.Forms.TextBox modelTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox precioTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox kmTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView carrosDataGridView;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
--- PrimerParcial/PrimerParcial/Ejercicio9.cs
+++ PrimerParcial/PrimerParcial/Ejercicio9.cs
@@ -17,11 +17,48 @@ namespace PrimerParcial
         {
             InitializeComponent();
         }
+        //Globales
+        Carro miCarro = null;//creando objeto vacio
+        List <Carro> listaCarros = new List <Carro>();//creando lista
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Carro miCarro = new Carro();//creando instancia u objeto de la clase "Carro", con el constructor vacio o "por default" en Java
-            Carro miCarro2 = new Carro("Subaru", "WRX");//utilizando constructor con parametros. Instanciar es crear un objeto de la clase (Carro en este caso)
+            //Carro miCarro = new Carro();//creando instancia u objeto de la clase "Carro", con el constructor vacio o "por default" en Java
+            //Carro miCarro2 = new Carro("Subaru", "WRX");//utilizando constructor con parametros. Instanciar es crear un objeto de la clase (Carro en este caso)
+
+            //Definiendo e inicializando variables
+            string marca = marcaTextBox.Text;
+            string modelo = modelTextBox.Text;
+            decimal precio = Convert.ToDecimal(precioTextBox.Text);
+            int km = Convert.ToInt32(kmTextBox.Text);
+
+            miCarro = new Carro();
+            miCarro.Marca = marca;
+            miCarro.Modelo = modelo;
+            miCarro.Precio = precio;
+            miCarro.Km= km;
+
+            //MessageBox.Show("Objeto Carro -> Marca: " + miCarro.Marca + " Modelo: " + miCarro.Modelo);
+            
+            listaCarros.Add(miCarro);//llenando lista mediante las modificaciones de los estados por el usuario
+            carrosDataGridView.DataSource = null;//limpiando 
+            carrosDataGridView.DataSource = listaCarros;
+            limpiarControles();
+            marcaTextBox.Focus();//enviando el foco al primer textbox luego de operar
+
+        }
+        private void limpiarControles()
+        {
+            //limpieando cajas de texto de 3 maneras:
+            marcaTextBox.Clear();
+            modelTextBox.Text = string.Empty;
+            precioTextBox.Text = "";
+            kmTextBox.Clear();
+        }
+        //mostrando el puntero del mouse en la primera textbox cuando inicia el programa
+        private void Ejercicio9_Load(object sender, EventArgs e)
+        {
+            marcaTextBox.Focus();
         }
     }
 }
