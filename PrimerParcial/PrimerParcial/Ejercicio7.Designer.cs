0bc45ff90707699c76d67120cc4b1fed848e0b9e
DavidAndino <123537730+DavidAndino@users.noreply.github.com>
2023-02-09 10:55:42 -0600

    Ejercicio 7 terminado

--- PrimerParcial/PrimerParcial/Ejercicio7.Designer.cs
+++ PrimerParcial/PrimerParcial/Ejercicio7.Designer.cs
@@ -30,6 +30,13 @@
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.filasTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.colsTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // listBox1
@@ -42,24 +49,88 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(224, 28);
+            this.button1.Location = new System.Drawing.Point(225, 111);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(121, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Generar Matriz";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(198, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tamaño  de Matriz: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(305, 161);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // filasTextBox
+            // 
+            this.filasTextBox.Location = new System.Drawing.Point(286, 28);
+            this.filasTextBox.Name = "filasTextBox";
+            this.filasTextBox.Size = new System.Drawing.Size(37, 20);
+            this.filasTextBox.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(225, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Filas";
+            // 
+            // colsTextBox
+            // 
+            this.colsTextBox.Location = new System.Drawing.Point(286, 66);
+            this.colsTextBox.Name = "colsTextBox";
+            this.colsTextBox.Size = new System.Drawing.Size(37, 20);
+            this.colsTextBox.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(225, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Columnas";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(369, 296);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 8;
             // 
             // Ejercicio7
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(542, 394);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.colsTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.filasTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
             this.Name = "Ejercicio7";
             this.Text = "Ejercicio7";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -67,5 +138,12 @@
 
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox filasTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox colsTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- PrimerParcial/PrimerParcial/Ejercicio7.cs
+++ PrimerParcial/PrimerParcial/Ejercicio7.cs
@@ -19,22 +19,46 @@ namespace PrimerParcial
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //Limpiando listBox para que no se vean luego de  dar click en el boton
+            listBox1.Items.Clear();
+
             //creando matriz e inicializandola
             int[,] matriz = new int[3, 3] { { 3,6,3}, { 6,9,7}, {8,9,4 } };//matriz 3x3 1er valor: filas, 2do: columnas
 
-            //creando otra matriz y llenandola randomly
-            int[,] matriz2 = new int[3, 4];//matriz de 2 dimensiones, solo se tiene filas (0, 1ra dimension) y cols (1, 2da dimension)
+            //creando instancia de la clase random para darle un numero aleatorio de dimensiones a la matriz
 
-            //creando instancia random
+            //Random dimensiones = new Random();
+
+            //int valorFila = dimensiones.Next(1, 10);//"Next" es  una funcion que genera un numero entero
+            //int valorCol = dimensiones.Next(1, 10);
+            //declarando e inicializando filas y cols con valores ingresados por  el usuario
+            int filas = Convert.ToInt32(filasTextBox.Text), cols = Convert.ToInt32(colsTextBox.Text);
+            
+            //creando otra matriz y llenandola randomly
+            int[,] matriz2 = new int[filas, cols];//matriz de 2 dimensiones, solo se tiene filas (0, 1ra dimension) y cols (1, 2da dimension)
+
+            //creando instancia random para llenar matriz
             Random generarNum = new Random();
 
-            for (int i = 0; i < matriz2.GetLength(0); i++)//ciclo de recorrido de filas
+            //llenando matriz 
+            for (int i = 0; i < matriz2.GetLength(0); i++)//ciclo de recorrido de dimension de filas
             {
-                for (int j = 0; j < matriz2.GetLength(1); j++)//ciclo de recorrido de columnas
+                for (int j = 0; j < matriz2.GetLength(1); j++)//ciclo de recorrido de dimension de columnas
                 {
-
+                    matriz2[i, j]= generarNum.Next(1,50);//generando valores del 1 al 50 que llenaran la matriz
                 }
             }
+
+            //creando ciclos para imprimir contenido de la matriz
+            for (int i = 0; i < matriz2.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz2.GetLength(1); j++)
+                {
+                    listBox1.Items.Add("El índice: [" + i + "," + j + "] = " + matriz2[i, j]);
+                }
+            }
+
+            textBox1.Text = matriz2.GetLength(0).ToString() + "x" + matriz2.GetLength(1).ToString();//imprimiendo dimension de matriz
         }
     }
 }
