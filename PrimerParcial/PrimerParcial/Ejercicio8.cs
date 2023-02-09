f2ba708ad11de5b45fb157aa0370e2f5be60c660
DavidAndino <123537730+DavidAndino@users.noreply.github.com>
2023-02-09 11:09:41 -0600

    ejercicio 8
    recursividad

--- PrimerParcial/PrimerParcial/Ejercicio8.cs
+++ PrimerParcial/PrimerParcial/Ejercicio8.cs
@@ -16,5 +16,21 @@ namespace PrimerParcial
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            recorrerDe1En1(1, 150);
+        }
+
+        //creando funcion recursiva
+        private void recorrerDe1En1(int valorInicial, int valorFinal)
+        {
+            listBox1.Items.Add(valorInicial);//llenando con el 1 (valor inicial)
+
+            if (valorInicial < valorFinal)
+            {
+                recorrerDe1En1(valorInicial +1, valorFinal);//sumando la  unidad al valor inicial mientras este siga siendo menor que el valor final
+            }
+        }
     }
 }
